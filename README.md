422ea080d0f17dbe129c5852100274f2aab06b01
Jean-Michel Bourget <jean-michel.bourget@equisoft.com>
2020-08-21 14:44:53 -0400

    Update readme & automate installation of requirements (Mac/Linux).

--- README.md
+++ README.md
@@ -6,18 +6,19 @@ Tool to beautify, color and ease the usage of the json received by `apps logs`.
 
 ![Logs before & after](https://i.gyazo.com/045c249b3895d7b7b3c950848c778baf.png)
 
-## Tips
-- To filter by app: `ezlogs | grep -i "docker.crm"`
-- To filter by log level: `ezlogs | grep -i "\[warning\]"`
-
 ## Requirements
-- jq (**Mac users**: you may need to install jq because it is not installed by default on mac `brew install jq`)
+- `apps logs` Equisoft/Connect stack
+- jq
 - awk
 - gsub
 
 ## Getting Started
 - Install ezlogs. *(See Installation section)*
-- Use ezlogs.
+- Use `ezlogs` in your terminal.
+
+## Tips
+- To filter by app: `ezlogs | grep -i "docker.crm"`
+- To filter by log level: `ezlogs | grep -i "\[warning\]"`
 
 ### Installation
 __Install__
--- install.sh
+++ install.sh
@@ -4,17 +4,38 @@
 readonly PATH_LINK=~/bin/ezlogs;
 
 if [[ $1 == "-r" ]]; then # Uninstall
-    if [[ -h $PATH_LINK ]]; then
-        rm $PATH_LINK;
-        echo "ezlogs uninstalled";
-    else
-        echo "ezlogs already uninstalled";
+  if [[ -h $PATH_LINK ]]; then
+    rm $PATH_LINK;
+    echo "ezlogs uninstalled";
+  else
+    echo "ezlogs already uninstalled";
+  fi
+else # Install
+  # Install requirements
+  if [ "$(uname)" == "Darwin" ]; then
+    if ! which jq > /dev/null; then
+      brew install jq
     fi
-else #Install
-    if [[ ! -h $PATH_LINK ]]; then
-        ln -s "`pwd`/ezlogs.sh" $PATH_LINK;
-        echo "ezlogs successfully installed";
-    else
-        echo "ezlogs already installed";
+    if ! which awk > /dev/null; then
+      brew install awk
     fi
+  fi
+  if [ "$(expr substr "$(uname -s)" 1 5)" == "Linux" ]; then
+    if ! which jq > /dev/null; then
+      sudo apt update
+      sudo apt install jq -y
+    fi
+    if ! which awk > /dev/null; then
+      sudo apt update
+      sudo apt install awk -y
+    fi
+  fi
+
+  # Install symlink to script
+  if [[ ! -h $PATH_LINK ]]; then
+    ln -s "`pwd`/ezlogs.sh" $PATH_LINK;
+    echo "ezlogs successfully installed";
+  else
+    echo "ezlogs already installed";
+  fi
 fi
